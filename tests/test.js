4a6754de07e7ff6dfaf7b455abb6a1ba850a2c49
Subin Sathish Kumar <subin.s@somaiya.edu>
2026-08-24 00:52:31 +0530

    Added Selenium test and Jenkinsfile

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,7 @@ const chrome = require("selenium-webdriver/chrome");
 require("chromedriver");
 
 async function runTest() {
+
     let options = new chrome.Options();
 
     let driver = await new Builder()
@@ -11,19 +12,25 @@ async function runTest() {
         .build();
 
     try {
+
         await driver.get("http://localhost:3000");
 
         await driver.wait(until.elementLocated(By.id("btn")), 10000);
 
         let button = await driver.findElement(By.id("btn"));
+
         await button.click();
 
         console.log("✅ Test Passed");
+
     } catch (err) {
-        console.error("❌ Test Failed");
-        console.error(err);
+
+        console.log(err);
+
     } finally {
+
         await driver.quit();
+
     }
 }
 
